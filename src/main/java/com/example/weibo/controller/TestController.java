f1659a732bba2104548b6b001d9d4f4bcc457588
SurtrLaevateinn <SurtrLaevateinn@SURTRLAEVATEINN>
2021-04-08 18:05:53 +0800

    0.1

--- src/main/java/com/example/weibo/controller/TestController.java
+++ src/main/java/com/example/weibo/controller/TestController.java
@@ -2,20 +2,34 @@ package com.example.weibo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class TestController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    @RequestMapping("/getUsers")
-    public List<Map<String,Object>> getDbType(){
-        String sql = "select * from userinfo";
-        List<Map<String,Object>> list = jdbcTemplate.queryForList(sql);
-        return list;
+    @PostMapping("/register")
+    public String register(@RequestParam("id") String id,
+                           @RequestParam("username") String username,
+                           @RequestParam("password") String password){
+        String sql = "insert into userinfo values (?,?,?);";
+        jdbcTemplate.update(sql,Integer.parseInt(id),username,password);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("id") String id,
+                        @RequestParam("password") String password){
+        String sql = "select * from userinfo where id=?;";
+        Map<String, Object> map = jdbcTemplate.queryForMap(sql);
+        if(password.equals(map.get("password")))
+            return "home";
+        else
+            return "login";
     }
 }
